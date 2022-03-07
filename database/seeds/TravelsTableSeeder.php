13a6fee4627201f6acd0d80be9ae82c9b86aa9ee
Masadavide <davide.masadm@gmail.com>
2022-03-07 17:19:28 +0100

    first commit

---
+++ database/seeds/TravelsTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use Faker\Generator as Faker;
+use App\Travel;
+
+class TravelsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 20; $i++){
+            $newTravel = new Travel();
+            $newTravel->destinazione = $faker->word();
+            $newTravel->partenza = $faker->date();
+            $newTravel->prezzo = $faker->numberBetween(200-500);
+            $newTravel->ritorno = $faker->date();
+            $newTravel->save();
+        }
+    }
+}
